1a6eeb50ad10aba59462492c85d3d61d0602bd0c
Muhammad Anas Seth <m.anasseth00@gmail.com>
2022-02-10 20:08:54 +0500

    Responsiveness Issue Fixed On Home Page

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { GlobalService } from 'src/app/service/global.service';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+// import { faSquareX } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -14,7 +17,91 @@ export class HeaderComponent implements OnInit {
   activatedLink: string = "";
   faHamburger = faBars;
   faGlobe = faGlobe;
-
+  faAngleRight = faAngleRight;
+  faCart = faCartPlus;
+  activeMenu = "Product";
+  showDropdownMobile: boolean = false
+  dropdownMenu = [
+    {
+      name: "Specification",
+      items: [
+        {
+          menuName: "3-Part CSI Guide",
+          class: "one"
+        },
+        {
+          menuName: "Lunch & Learn",
+          class: "one"
+        },
+        {
+          menuName: "Revit Files",
+          class: "one"
+        },
+        {
+          menuName: "Technical Documents",
+          class: "two"
+        },
+        {
+          menuName: "Brochure",
+          class: "two"
+        },
+        {
+          menuName: "Fabrication Videos",
+          class: "two"
+        },
+      ]
+    },
+    {
+      name: "Product",
+      items: [
+        {
+          menuName: "Project",
+          class: "one"
+        },
+        {
+          menuName: "Franchise",
+          class: "one"
+        },
+        {
+          menuName: "Commercial",
+          class: "one"
+        },
+        {
+          menuName: "Education",
+          class: "two"
+        },
+        {
+          menuName: "HealthCare",
+          class: "two"
+        },
+      ]
+    },
+    {
+      name: "Inspiration",
+      items: [
+        {
+          menuName: "Project",
+          class: "one"
+        },
+        {
+          menuName: "Franchise",
+          class: "one"
+        },
+        {
+          menuName: "Commercial",
+          class: "one"
+        },
+        {
+          menuName: "Education",
+          class: "two"
+        },
+        {
+          menuName: "HealthCare",
+          class: "two"
+        },
+      ]
+    }
+  ]
   constructor(public global: GlobalService) { }
 
   ngOnInit(): void {
@@ -22,6 +109,18 @@ export class HeaderComponent implements OnInit {
 
   showCountryPopup() {
     this.global.showPopup = true
+  }
+
+  switchMenu(keyName: string) {
+    console.log(keyName)
+    this.activeMenu = keyName
+    this.getMenuItems();
+  }
+
+  getMenuItems() {
+    console.log("KeyName : ", this.activeMenu)
+    console.log(this.dropdownMenu.filter(x => x.name.toLowerCase().toString() == this.activeMenu.toLowerCase().toString())[0].items)
+    return this.dropdownMenu.filter(x => x.name.toLowerCase().toString() == this.activeMenu.toLowerCase().toString())[0].items
   }
 
   showRegionPopup() {
