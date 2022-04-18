c4b0743597260330cf836910228b85e736218222
Muhammad Anas Seth <m.anasseth00@gmail.com>
2022-04-18 15:11:10 +0500

    Restore Scroll Position To Top

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -60,8 +60,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  // imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
+  // imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
